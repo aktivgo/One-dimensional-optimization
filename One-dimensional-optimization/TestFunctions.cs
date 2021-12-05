3dab340522393822b8b9642d349b754232ecb2d9
aktivgo <AktivGo01062001@gmail.com>
2021-12-05 11:16:29 +0300

    refactoring test functions

--- One-dimensional-optimization/TestFunctions.cs
+++ One-dimensional-optimization/TestFunctions.cs
@@ -4,7 +4,8 @@ namespace One_dimensional_optimization
 {
     public static class TestFunctions
     {
-        // x^4 + e^(-x)
+        #region x^4 + e^(-x)
+
         public static double TestFunc1GetValue(double x)
         {
             return Math.Pow(x, 4) + Math.Pow(Math.E, -x);
@@ -15,20 +16,20 @@ namespace One_dimensional_optimization
             return "x^4 + e^(-x)";
         }
 
-        // -x^4 - e^(-x)
         public static double TestInverseFunc1GetValue(double x)
         {
             return -Math.Pow(x, 4) - Math.Pow(Math.E, -x);
         }
 
+        #endregion
 
-        // x(1-x^2)^(1/3)
+        #region x(1-x^2)^(1/3)
+
         public static double TestFunc2GetValue(double x)
         {
             return x * Math.Pow(1 - Math.Pow(x, 2), (double)1 / 3);
         }
 
-        // -x(1-x^2)^(1/3)
         public static double TestInverseFunc2GetValue(double x)
         {
             return -x * Math.Pow(1 - Math.Pow(x, 2), (double)1 / 3);
@@ -38,5 +39,7 @@ namespace One_dimensional_optimization
         {
             return "x(1-x^2)^(1/3)";
         }
+
+        #endregion
     }
 }
